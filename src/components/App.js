a0d2af00da8ff95b2523844f84cdfb1333ea1ed5
Piotr Świstowski <swistekxd@gmail.com>
2021-10-13 19:51:25 +0200

    well done

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 class App extends React.Component{
-state= {videos: [] };
+state= {videos: [], selectedVideo:null };
 
+componentDidMount(){
+  this.onTermSubmit("Charlie bit my finger! ORIGINAL")
+}
 
   onTermSubmit= async term=>{
   const response =  await youtube.get("/search",{
@@ -14,15 +18,35 @@ state= {videos: [] };
     });
 
     
-    this.setState({videos: response.data.items})
-    console.log(this.state.videos)
+    this.setState({
+
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
+    // console.log(this.state.videos)
   };
+
+  onVideoSelect=(video)=> {
+    this.setState({selectedVideo:video})
+  }
 
   render(){
     return(
       <div className="ui container">
 <SearchBar onFormSubmit={this.onTermSubmit}/>
-      <VideoList videos={this.state.videos}/>
+
+<div className="ui grid"> 
+<div className="ui row">
+<div className="ten wide column">
+  
+      <VideoDetail video={this.state.selectedVideo}/>
+</div>
+<div className="sex wide column">
+
+      <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+</div>
+</div>
+</div>
       </div>
     )
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,13 +1,21 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList= (props) => {
+
+const VideoList= ({videos, onVideoSelect}) => {
+const renderList = videos.map((video)=>{
+  return(
+    <VideoItem  key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />
+  )
+})
+  
   //props.videos
   return(
-    <div>
-      {props.videos.length}
+    <div className="ui relaxed divided list">
+      {renderList}
     </div>
 
   )
 }
-//aaaaaa
+
 export default VideoList;
